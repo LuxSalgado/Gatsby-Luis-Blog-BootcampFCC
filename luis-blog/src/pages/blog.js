00545908d82296305cbf65393f57f0c4e883fec0
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-09-09 18:19:23 +0000

    Obteniendo datos del File System y trabajando con Markdown Posts

--- luis-blog/src/pages/blog.js
+++ luis-blog/src/pages/blog.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>Blog Page</h1>
-            <p>Aqui se van a mostrar las entradas a los blogs</p>
+            <ol>
+                {data.allMarkdownRemark.edges.map( (articulo)=>{
+                    return (<li>
+                        <h2>{articulo.node.frontmatter.title}</h2>
+                        <p>{articulo.node.frontmatter.date}</p>
+                    </li>)
+                })}
+            </ol>
         </Layout>
     )
 }
---
+++ luis-blog/src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "Perritos"
+date: "2021-09-09"
+---
+
+I just launched a new bootcamp
+
+## Topics covered
+
+1. Uno
+2. Dos
+3. Otro ps
